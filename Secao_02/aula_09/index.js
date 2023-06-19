40e4454d572f19b3cf0794fbeb8f84f83acfb41d
Leonardo Santos <leonardojosantos@gmail.com>
2023-06-18 22:59:36 -0300

    Diferenças entre LET e VAR.

---
+++ Secao_02/aula_09/index.js
@@ -0,0 +1,15 @@
+
+// NÃO UTILIZAR VAR POIS ACEITA REDECLARAR
+var declaracaoComVar = 2
+console.log(declaracaoComVar);
+
+var declaracaoComVar = 4
+console.log(declaracaoComVar);
+
+// UTILIZAR LET POIS NÃO ACEITA REDECLARAR
+let declaracaoComLet = 5
+console.log(declaracaoComLet);
+
+let declaracaoComLet = 4
+console.log(declaracaoComLet);
+
