0aa11eb38c4210cc4124563c61ca87e57d57951f
Leonardo Santos <leonardojosantos@gmail.com>
2023-06-19 20:59:54 -0300

    Operadores de comparação.

---
+++ Secao_03-JavaScript_logica_de_programacao/aula_21-Operadores_de_comparacao/script.js
@@ -0,0 +1,53 @@
+/*
+Operadores de comparação
+>   maior que
+<   menor que
+>=  maior ou igual
+<=  menor ou igual
+==  igualdade (valor) - não é indicado usar
+=== igualdade estrito (valor e tipo) - indicado usar
+!=  diferente (valor) - não é indicado usar
+!== diferente estrito (valor e tipo) - indicado usar
+*/
+
+console.log();
+
+const num1 = 10;
+const num2 = '10';
+const comp = (num1 !== num2);
+console.log(comp);
+
+// const num1 = 10;
+// const num2 = 10;
+// const comp = (num1 != num2);
+// console.log(comp);
+
+// const num1 = 10;
+// const num2 = '10';
+// const comp = (num1 === num2);
+// console.log(comp);
+
+// const num1 = 10;
+// const num2 = 10
+// const comp = (num1 == num2);
+// console.log(comp);
+
+// const num1 = 10;
+// const num2 = 10
+// const comp = (num1 > num2);
+// console.log(comp);
+
+// const num1 = 10;
+// const num2 = 10
+// const comp = (num1 < num2);
+// console.log(comp);
+
+// const num1 = 10;
+// const num2 = 10
+// const comp = (num1 >= num2);
+// console.log(comp);
+
+// const num1 = 10;
+// const num2 = 10
+// const comp = (num1 <= num2);
+// console.log(comp);
