68d1fe6df88b9756e078263f7a3370014898ba5d
Leonardo Santos <leonardojosantos@gmail.com>
2023-06-19 00:05:13 -0300

    Operadores: Aritméticos, atribuição e incremento.

---
+++ Secao_02/aula_11/index.js
@@ -0,0 +1,104 @@
+/*
+    Operadore aritmeticos  
+        +       
+        -       
+        /           
+        *           
+        **          
+        %
+
+    Ordem de precedencia
+        ()      
+        **      
+        * / %       
+        * + - 
+*/
+/*
+const numero = 10;
+const outroNumero = 2;
+
+const adicao = numero + outroNumero;
+const subtracao = numero - outroNumero;
+const divisao = numero / outroNumero;
+const multiplicacao = numero * outroNumero;
+const exponenciacao = numero ** outroNumero;
+const resto = numero % outroNumero;
+
+console.log(`${adicao}`);
+console.log(`${subtracao}`);
+console.log(`${divisao}`);
+console.log(`${multiplicacao}`);
+console.log(`${exponenciacao}`);
+console.log(`${resto}`);
+console.log('\n-----------------------------------------------------------------\n');
+*/
+
+/*
+    Operador de incremento  ++ 
+    Operador de decremento  --      
+*/
+/*
+let posIncremento = 10;
+console.log(posIncremento);
+console.log(posIncremento++);
+console.log(posIncremento);
+
+console.log();
+
+let preIncremento = 10;
+console.log(preIncremento);
+console.log(++preIncremento);
+console.log(preIncremento);
+
+let posDecremento = 10;
+console.log(posDecremento);
+console.log(posDecremento--);
+console.log(posDecremento);
+
+console.log();
+
+let preDecremento = 10;
+console.log(preDecremento);
+console.log(--preDecremento);
+console.log(preDecremento);
+console.log('\n-----------------------------------------------------------------');
+*/
+
+/*
+    Operadpres de atribuição
+    +=
+    -=
+    *=
+    /=
+*/
+
+let contador = 2
+
+contador += 2;
+console.log(contador);
+
+contador -= 2;
+console.log(contador);
+
+contador *= 2;
+console.log(contador);
+
+contador /= 2;
+console.log(contador);
+
+const num1 = 10;
+const num2 = '5';
+console.log(typeof(num1 * num2));
+console.log(typeof (num1 + num2));
+
+const num3 = 10;
+const num4 = parseInt('5.6');
+console.log(num3 + num4);
+
+const num5 = 10;
+const num6 = parseFloat('5.6');
+console.log(num5 + num6);
+
+const num7 = 10;
+const num8 = Number('5.6');
+console.log(num7 + num8);
