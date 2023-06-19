378d2fbe4cb9f2483b71953799c9959031a08b6a
Leonardo Santos <leonardojosantos@gmail.com>
2023-06-19 14:43:32 -0300

    Arrays - Básico

---
+++ Secao_02/aula_16-Arrays/script.js
@@ -0,0 +1,62 @@
+console.log();
+
+const alunos = ['Luiz', 'Maria', 'João'];
+console.log(alunos);
+
+console.log(typeof (alunos));
+console.log(alunos instanceof Array);
+
+/*
+alunos.push('Luiza');
+alunos.push('Eduardo');
+console.log(alunos.slice(0, 3));
+console.log(alunos.slice(0, -2));
+*/
+
+// console.log(alunos[50]); // Se o elemento não existir, retorna undefined
+
+/*
+// delete alunos[1]; //deleta o índice
+// console.log(alunos[1]);
+// console.log(alunos);
+*/
+
+/*
+// const ultimoAlunoRemovido = alunos.pop(); // remove o último elemento
+// console.log(ultimoAlunoRemovido);
+// console.log(alunos);
+
+// const primeiroAlunoRemovido = alunos.shift(); // remove o primeiro elemento
+// console.log(primeiroAlunoRemovido);
+// console.log(alunos);
+*/
+
+/*
+// alunos.unshift('Luiza'); // adiciona no início
+// alunos.unshift('Fábio'); // adiciona no início
+// console.log(alunos);
+*/
+
+/*
+// alunos.push('Luiza'); // adiciona no final
+// alunos.push('Fábio'); // adiciona no final
+// console.log(alunos);
+*/
+
+/*
+// alunos[alunos.length] = 'Luiza'
+// alunos[alunos.length] = 'Fábio'
+// alunos[alunos.length] = 'Luana'
+// console.log(alunos);
+*/
+
+/*
+// alunos[0] = 'Eduardo';
+// alunos[3] = 'Luiza';
+// console.log(`${alunos}`);
+*/
+
+/*
+// console.log(`${alunos}`);
+// console.log(`${alunos[0][3]}`);
+*/
