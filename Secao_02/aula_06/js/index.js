01ed304f5fe3c3120608f2a334f578fcbd6573ad
Leonardo Santos <leonardojosantos@gmail.com>
2023-06-18 22:16:25 -0300

    Variáveis com LET.

---
+++ Secao_02/aula_06/js/index.js
@@ -0,0 +1,12 @@
+
+/**
+ * Regras para declaração de variáveis
+ *  - Não utilizar palavras reservadas.
+ *  - Utilizar nomes significativos.
+ *  - Não começar com número.
+ *  - Não utilizar espaços.
+ *  - Utilizar camelCase
+ *  - Ter cuidado com Case-sensitive
+ *  - Variáveis do tipo LET não podem ser redeclaradas
+ *  - Não utilize VAR (antigo), utilize LET.
+ */
