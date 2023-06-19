2ce898812728ec8168825bb5c99a1c487dba613b
Leonardo Santos <leonardojosantos@gmail.com>
2023-06-18 23:13:29 -0300

    Tipos de dados primitivos.

---
+++ Secao_02/aula_10/index.js
@@ -0,0 +1,19 @@
+// string
+const nome = 'Leonardo';
+console.log(`Tipo de dado: ${typeof(nome)}`);
+
+// number
+const numero = 1;
+console.log(`Tipo de dado: ${typeof (numero)}`);
+
+// undefined
+let nomeAluno;
+console.log(`Tipo de dado: ${typeof (nomeAluno)}`);
+
+// object
+const valorNulo = null;
+console.log(`Tipo de dado: ${typeof (valorNulo)}`);
+
+// boolean
+const trabalhando = true;
+console.log(`Tipo de dado: ${typeof (trabalhando)}`);
