b9f49b90feb64abab2af6ecfa89407ff8baa8760
Leonardo Santos <leonardojosantos@gmail.com>
2023-06-19 01:00:13 -0300

    Exercício 03

---
+++ Secao_02/aula_12/js/scriptExercicio.js
@@ -0,0 +1,10 @@
+// Exercicio
+let primeiroNumero = Number(prompt('Digite o primeiro número'));
+let segundoNumero = Number(prompt('Digite o segundo número'));
+
+// const resultadoDaSoma = primeiroNumero + segundoNumero;
+
+alert(`A soma de ${primeiroNumero} e ${segundoNumero} é ${primeiroNumero + segundoNumero}`);
+
+console.log(primeiroNumero + segundoNumero);
+console.log(typeof(primeiroNumero + segundoNumero));
