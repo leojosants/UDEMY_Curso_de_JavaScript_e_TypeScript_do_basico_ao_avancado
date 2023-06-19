4e705b3f93d3beab22abe714d203e40aa35ad16c
Leonardo Santos <leonardojosantos@gmail.com>
2023-06-19 12:45:16 -0300

    Objeto MATH.

---
+++ Secao_02/aula_15-Math/index.js
@@ -0,0 +1,34 @@
+/*
+// let num1 = 9.49;
+// let num2 = Math.floor(num1); // Arredonda para baixo
+// let num2 = Math.ceil(num1); // Arredonda para cima
+// let num2 = Math.round(num1); // até .49 arredonda para baixo, a partir de 50 arredonda pra cima
+// console.log(`\n${num1}`);
+// console.log(`${num2}`);
+*/
+
+/*
+// console.log(Math.max(1, 5, -3, 1234, -344, 6.9, 34));
+// console.log(Math.min(1, 5, -3, 1234, -344, 6.9, 34));
+// console.log(Math.PI);
+// console.log(Math.pow(2, 10));
+*/
+
+/*
+// const numeroAleatorio = Math.random() * (10 - 5) + 5; // números aleatório com intervado
+// console.log(`\n${numeroAleatorio}`);
+
+// const numeroAleatorio = Math.round(Math.random() * (10 - 5) + 5); // números aleatório com intervado - arredondando
+// console.log(`\n${numeroAleatorio}`);
+*/
+
+/*
+// Raiz quadrada
+let num1 = 16;
+console.log(num1 ** 0.5);
+console.log(Math.sqrt(16));
+*/
+
+// E JS é aceitavel divisão por ZERO
+console.log(10 / 0);
+console.log(10 / 0.0001);
