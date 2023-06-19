92cd7fea90ada033c7958560962dc7a7f1eb49d3
Leonardo Santos <leonardojosantos@gmail.com>
2023-06-18 22:26:59 -0300

    Constantes com CONST.

---
+++ Secao_02/aula_07/js/index.js
@@ -0,0 +1,12 @@
+
+/**
+ * Regras para declaração de constantes
+ *  - Não utilizar palavras reservadas.
+ *  - Utilizar nomes significativos.
+ *  - Não começar com número.
+ *  - Não utilizar espaços.
+ *  - Utilizar camelCase
+ *  - Ter cuidado com Case-sensitive
+ *  - Constantes não podem ser alteradas
+ *  - Não utilize VAR (antigo), utilize CONST.
+ */
