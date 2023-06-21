808445dba2d06673d5e12b9e3db17d3dc85e5b17
Leonardo Santos <leonardojosantos@gmail.com>
2023-06-21 15:38:02 -0300

    Exercício com lógica de programação - 02.

---
+++ Secao_03-JavaScript_logica_de_programacao/aula_42-Exercicio_02_logica/script.js
@@ -0,0 +1,9 @@
+
+/* 
+    Escreva uma função chamada 'ePaisagem' que recebe dois argumentos, largura e altura de uma imagem(number).
+    Retorne 'true' se a imagem estiver no modo paisagem
+*/
+
+const ePaisagem = (largura, altura) => largura > altura;
+
+console.log(ePaisagem(Math.floor(Math.random() * 5000), Math.random() * 5000));
