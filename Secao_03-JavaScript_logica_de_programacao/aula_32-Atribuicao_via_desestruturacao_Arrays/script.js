8ebed13c96f6a8f618510a3853d379adb7f0a5cf
Leonardo Santos <leonardojosantos@gmail.com>
2023-06-20 22:06:31 -0300

    Atribuição via desestruturação (Arrays).

---
+++ Secao_03-JavaScript_logica_de_programacao/aula_32-Atribuicao_via_desestruturacao_Arrays/script.js
@@ -0,0 +1,34 @@
+// 
+const numeros = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8,]
+];
+
+const [lista1, lista2, lista3] = numeros;
+
+console.log(lista3[0]);
+
+/*
+// const numeros = [
+//     [0, 1, 2], [3, 4, 5], [6, 7, 8,]
+// ];
+// 
+// console.log(numeros[1][2]);
+*/
+
+/*
+// const numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const [zero, um, , tres, , , seis] = numeros;
+
+// console.log(um);
+// console.log(tres);
+// console.log(seis);
+*/
+
+/*
+// const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const [um, dois, ...resto] = numeros;
+
+// console.log(um);
+// console.log(dois);
+// console.log(resto);
+*/
