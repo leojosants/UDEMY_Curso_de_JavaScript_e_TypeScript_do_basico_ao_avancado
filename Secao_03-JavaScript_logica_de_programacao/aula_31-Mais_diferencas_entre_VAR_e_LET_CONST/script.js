54d649214e30d4655d5064d196f0c2267b6df974
Leonardo Santos <leonardojosantos@gmail.com>
2023-06-20 21:18:18 -0300

    Mais diferenças entre VAR e LET/CONST.

---
+++ Secao_03-JavaScript_logica_de_programacao/aula_31-Mais_diferencas_entre_VAR_e_LET_CONST/script.js
@@ -0,0 +1,54 @@
+
+/*
+// console.log(sobrenome); // Ocorrerá erro, porém reconhece que foi inicialidada, porem chamada antes de ser declarada
+// var sobrenome = 'Leo';
+*/
+
+/*
+// console.log(sobrenome2); // erro, não permite executar sendo inicializada depois
+// let sobrenome2 = 'Leonardo';
+*/
+
+/* 
+LET tem escopo de bloco
+VAR é tem escopo de função
+*/
+
+/*
+// const verdadeira = true;
+
+// falaOi = () => {
+
+//     if (verdadeira) {
+//         let nome = 'Leo';
+//         var sobrenome = 'Santos';
+//     }
+
+//     console.log(sobrenome);
+// }
+
+// // console.log(nome); //não executará
+
+// falaOi();
+*/
+
+/*
+// const verdadeira = true;
+
+// let nome = 'Leonardo';
+// var nome2 = 'José';
+
+// if (verdadeira) {
+
+//     let nome = 'Luan';
+//     var nome2 = 'João'
+
+//     if (verdadeira) {
+
+//         var nome2 = 'Santos';
+//         let nome = 'Maria';
+//     }
+// }
+
+// console.log(nome, nome2);
+*/
