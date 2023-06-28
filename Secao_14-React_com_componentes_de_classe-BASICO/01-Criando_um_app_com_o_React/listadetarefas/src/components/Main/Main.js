ddcd71a82c2620c06ba99a69ef59b2c4c0ebcfd6
Leonardo Santos <leonardojosantos@gmail.com>
2023-06-27 23:29:04 -0300

    Estilizando as tarefas.

--- Secao_14-React_com_componentes_de_classe-BASICO/01-Criando_um_app_com_o_React/listadetarefas/src/components/Main/Main.js
+++ Secao_14-React_com_componentes_de_classe-BASICO/01-Criando_um_app_com_o_React/listadetarefas/src/components/Main/Main.js
@@ -4,9 +4,17 @@ import './Main.css';
 // Form
 import { FaPlus } from 'react-icons/fa';
 
+// Tarefas
+import { FaEdit, FaWindowClose } from 'react-icons/fa';
+
 export default class Main extends Component {
   state = {
     novaTarefa: '',
+    tarefas: [
+      'Fazer café',
+      'Beber água',
+      'Estudar',
+    ],
   };
 
   handleChange = (e) => {
@@ -16,7 +24,7 @@ export default class Main extends Component {
   };
 
   render() {
-    const { novaTarefa } = this.state;
+    const { novaTarefa, tarefas } = this.state;
 
     return (
       <div className="main">
@@ -33,6 +41,18 @@ export default class Main extends Component {
             <FaPlus />
           </button>
         </form>
+
+        <ul className="tarefas">
+          {tarefas.map((tarefa) => (
+            <li key={tarefa}>
+              {tarefa}
+              <div>
+                <FaEdit className="edit" />
+                <FaWindowClose className="delete" />
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
