0fcad965e67a95dee788a11a0730dcc6fac2507c
Leonardo Santos <leonardojosantos@gmail.com>
2023-06-28 10:24:48 -0300

    Apagando tarefas.

--- Secao_14-React_com_componentes_de_classe-BASICO/01-Criando_um_app_com_o_React/listadetarefas/src/components/Main/Main.js
+++ Secao_14-React_com_componentes_de_classe-BASICO/01-Criando_um_app_com_o_React/listadetarefas/src/components/Main/Main.js
@@ -34,6 +34,20 @@ export default class Main extends Component {
     });
   };
 
+  handleEdit = (e, index) => {
+    console.log('Edit', index);
+  };
+
+  handleDelete = (e, index) => {
+    const { tarefas } = this.state;
+    const novasTarefas = [...tarefas];
+    novasTarefas.splice(index, 1);
+
+    this.setState({
+      tarefas: [...novasTarefas],
+    });
+  };
+
   render() {
     const { novaTarefa, tarefas } = this.state;
 
@@ -54,12 +68,19 @@ export default class Main extends Component {
         </form>
 
         <ul className="tarefas">
-          {tarefas.map((tarefa) => (
+          {tarefas.map((tarefa, index) => (
             <li key={tarefa}>
               {tarefa}
               <span>
-                <FaEdit className="edit" />
-                <FaWindowClose className="delete" />
+                <FaEdit
+                  className="edit"
+                  onClick={(e) => this.handleEdit(e, index)}
+                />
+
+                <FaWindowClose
+                  className="delete"
+                  onClick={(e) => this.handleDelete(e, index)}
+                />
               </span>
             </li>
           ))}
