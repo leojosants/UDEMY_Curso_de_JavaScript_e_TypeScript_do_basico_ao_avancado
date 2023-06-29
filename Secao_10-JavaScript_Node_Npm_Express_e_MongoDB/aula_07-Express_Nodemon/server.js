5f8b6e2b92346455df21adaf869b4b1c6ce1002d
Leonardo Santos <leonardojosantos@gmail.com>
2023-06-29 20:31:44 -0300

    Express - Nodemon.

---
+++ Secao_10-JavaScript_Node_Npm_Express_e_MongoDB/aula_07-Express_Nodemon/server.js
@@ -0,0 +1,25 @@
+// 
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(`
+        <form action="/" method="POST">
+            Nome do cliente: <input type="text" name="nome"/>
+            <button>Enviar</button>
+        </form>
+    `);
+});
+
+app.post('/', (req, res) => {
+    res.send('Recebi o formuário!');
+});
+
+app.get('/contato', (req, res) => {
+    res.send('Página de contato!');
+});
+
+app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor executando na porta 3000!');
+});
