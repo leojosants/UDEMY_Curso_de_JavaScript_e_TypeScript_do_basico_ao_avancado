1954673312eea36a7e2e9876dc7e00f83be0a23c
Leonardo Santos <leonardojosantos@gmail.com>
2023-07-01 16:26:17 -0300

    Atualizado estrutura de pastas.

--- Secao_10-JavaScript_Node_Npm_Express_e_MongoDB/aula_15-Express_Session_e_Flash_Messages/src/middlewares/middleware.js
+++
@@ -1,8 +0,0 @@
-// Não esquecer do NEXT dentro do middleware, senão a função nao termina
-exports.middlewareGlobal = (req, res, next) => {
-    next();
-};
-
-exports.outroMiddleware = (req, res, next) => {
-    next();
-};
