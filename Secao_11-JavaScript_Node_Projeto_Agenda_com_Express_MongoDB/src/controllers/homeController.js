47a26dc898115e2d38666efdd6ccd8065329d38b
Leonardo Santos <leonardojosantos@gmail.com>
2023-07-02 11:24:33 -0300

    Criando a página de Login / Criar conta - Parte 1

--- Secao_11-JavaScript_Node_Projeto_Agenda_com_Express_MongoDB/src/controllers/homeController.js
+++ Secao_11-JavaScript_Node_Projeto_Agenda_com_Express_MongoDB/src/controllers/homeController.js
@@ -1,13 +1,4 @@
 //
-exports.paginaInicial = (req, res) => {
-    res.render('index', {
-        titulo: 'Título da página!',
-        numeros: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    });
-    return;
-};
-
-exports.trataPost = (req, res) => {
-    res.send(req.body);
-    return;
+exports.index = (req, res) => {
+    res.render('index');
 };
