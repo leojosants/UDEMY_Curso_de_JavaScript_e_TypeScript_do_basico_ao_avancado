3a98d4360fda0ab4f2149996e08e6c271404658b
Leonardo Santos <leonardojosantos@gmail.com>
2023-07-01 21:12:17 -0300

    Refatorado arquivo 'server.js'

--- Secao_10-JavaScript_Node_Npm_Express_e_MongoDB/aula_17-Helmet_e_CSRF/server.js
+++ Secao_10-JavaScript_Node_Npm_Express_e_MongoDB/aula_17-Helmet_e_CSRF/server.js
@@ -22,6 +22,7 @@ const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/midd
 
 app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 // Configuração da Sessão
@@ -38,8 +39,10 @@ const sessionOptions = session({
 
 app.use(sessionOptions);
 app.use(flash());
+
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
+
 app.use(csrf());
 app.use(middlewareGlobal);
 app.use(checkCsrfError);
