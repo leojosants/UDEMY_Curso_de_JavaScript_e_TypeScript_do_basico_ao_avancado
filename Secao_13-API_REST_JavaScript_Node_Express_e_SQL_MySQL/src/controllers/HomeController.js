5abc7aaefeaf52df38d198ea531e0311d49f8625
Leonardo Santos <leonardojosantos@gmail.com>
2023-07-06 17:09:50 -0300

    Configurando e testando o Sequelize - Parte 2

--- Secao_13-API_REST_JavaScript_Node_Express_e_SQL_MySQL/src/controllers/HomeController.js
+++ Secao_13-API_REST_JavaScript_Node_Express_e_SQL_MySQL/src/controllers/HomeController.js
@@ -1,8 +1,17 @@
+import Aluno from '../models/Aluno';
+
 class HomeController {
-  index(req, res) {
-    res.status(401).json({
-      tudoCerto: true,
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      nome: 'Júlia',
+      sobrenome: 'Josué',
+      email: 'juliajosues@gmail.com',
+      idade: 39,
+      peso: 75,
+      altura: 1.70,
     });
+
+    res.json(novoAluno);
   }
 }
 
