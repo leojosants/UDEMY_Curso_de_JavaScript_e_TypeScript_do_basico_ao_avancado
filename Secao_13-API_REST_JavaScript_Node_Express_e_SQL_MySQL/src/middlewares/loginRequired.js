1c92260c42c4257019741f035697c697ad88f306
Leonardo Santos <leonardojosantos@gmail.com>
2023-07-07 20:25:37 -0300

    Middleware de verificação do JWT

---
+++ Secao_13-API_REST_JavaScript_Node_Express_e_SQL_MySQL/src/middlewares/loginRequired.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken';
+
+export default (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).json({ errors: ['É necessário fazer LOGIN!'] });
+  }
+
+  const [, token] = authorization.split(' ');
+
+  try {
+    const dados = jwt.verify(token, process.env.TOKEN_SECRET);
+    const { id, email } = dados;
+
+    req.userId = id;
+    req.userEmail = email;
+
+    return next();
+  } catch (e) {
+    console.log(e);
+    return res.status(401).json({ errors: ['TOKEN expirado ou inválido!'] });
+  }
+};
