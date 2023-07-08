60b4438d227237379e4da7f49a412fcb0d436a88
Leonardo Santos <leonardojosantos@gmail.com>
2023-07-07 22:58:53 -0300

    Correções de segurança para usuários

--- Secao_13-API_REST_JavaScript_Node_Express_e_SQL_MySQL/src/controllers/UserController.js
+++ Secao_13-API_REST_JavaScript_Node_Express_e_SQL_MySQL/src/controllers/UserController.js
@@ -5,7 +5,8 @@ class UserController {
   async store(req, res) {
     try {
       const novoUser = await User.create(req.body);
-      return res.json(novoUser);
+      const { id, nome, email } = novoUser;
+      return res.json({ id, nome, email });
     } catch (e) {
       return res.status(400).json({
         errors: e.errors.map((err) => err.message),
@@ -16,11 +17,7 @@ class UserController {
   // Index: lista todos os usuários -> GET
   async index(req, res) {
     try {
-      const users = await User.findAll();
-
-      console.log('USER ID:', req.userId);
-      console.log('USER EMAIL:', req.userEmail);
-
+      const users = await User.findAll({ attributes: ['id', 'nome', 'email'] });
       return res.json(users);
     } catch (e) {
       return res.json(null);
@@ -31,7 +28,8 @@ class UserController {
   async show(req, res) {
     try {
       const user = await User.findByPk(req.params.id);
-      return res.json(user);
+      const { id, nome, email } = user;
+      return res.json({ id, nome, email });
     } catch (e) {
       return res.json(null);
     }
@@ -40,18 +38,15 @@ class UserController {
   // Update: atualiza um usuário -> PATCH OU PUT
   async update(req, res) {
     try {
-      if (!req.params.id) {
-        return res.status(400).json({ errors: ['ID não enviado!'] });
-      }
-
-      const user = await User.findByPk(req.params.id);
+      const user = await User.findByPk(req.params.userId);
 
       if (!user) {
-        return res.status(400).json({ errors: ['USUÁRIO não existe!'] });
+        return res.status(400).json({ errors: ['--> USUÁRIO não existe!'] });
       }
 
       const novosDados = await user.update(req.body);
-      return res.json(novosDados);
+      const { id, nome, email } = novosDados;
+      return res.json({ id, nome, email });
     } catch (e) {
       return res.status(400).json({ errors: e.errors.map((err) => err.message) });
     }
@@ -60,17 +55,13 @@ class UserController {
   // Delete: deleta um usuário -> DELETE
   async delete(req, res) {
     try {
-      if (!req.params.id) {
-        return res.status(400).json({ errors: ['ID não enviado!'] });
-      }
-
-      const user = await User.findByPk(req.params.id);
+      const user = await User.findByPk(req.params.userId);
 
       if (!user) {
-        return res.status(400).json({ errors: ['USUÁRIO não existe!'] });
+        return res.status(400).json({ errors: ['--> USUÁRIO não existe!'] });
       }
       await user.destroy();
-      return res.json(user);
+      return res.json(null);
     } catch (e) {
       return res.status(400).json({ errors: e.errors.map((err) => err.message) });
     }
--- Secao_13-API_REST_JavaScript_Node_Express_e_SQL_MySQL/src/middlewares/loginRequired.js
+++ Secao_13-API_REST_JavaScript_Node_Express_e_SQL_MySQL/src/middlewares/loginRequired.js
@@ -1,10 +1,11 @@
 import jwt from 'jsonwebtoken';
+import User from '../models/User';
 
-export default (req, res, next) => {
+export default async (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization) {
-    return res.status(401).json({ errors: ['É necessário fazer LOGIN!'] });
+    return res.status(401).json({ errors: ['--> É necessário fazer LOGIN!'] });
   }
 
   const [, token] = authorization.split(' ');
@@ -12,13 +13,18 @@ export default (req, res, next) => {
   try {
     const dados = jwt.verify(token, process.env.TOKEN_SECRET);
     const { id, email } = dados;
+    const user = await User.findOne({ where: { id, email } });
+
+    if (!user) {
+      return res.status(401).json({ errors: ['--> USUÁRIO inválido!'] });
+    }
 
     req.userId = id;
     req.userEmail = email;
 
     return next();
   } catch (e) {
-    console.log(e);
-    return res.status(401).json({ errors: ['TOKEN expirado ou inválido!'] });
+    // console.log(e);
+    return res.status(401).json({ errors: ['--> TOKEN expirado ou inválido!'] });
   }
 };
