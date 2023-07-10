0e2dae5b742080d8e3e75a59d633d430fcd7bbe0
Leonardo Santos <leonardojosantos@gmail.com>
2023-07-09 21:59:04 -0300

    Arquivos estáticos

--- Secao_13-API_REST_JavaScript_Node_Express_e_SQL_MySQL/src/controllers/AlunoController.js
+++ Secao_13-API_REST_JavaScript_Node_Express_e_SQL_MySQL/src/controllers/AlunoController.js
@@ -9,7 +9,7 @@ class AlunoController {
         order: [['id', 'DESC'], [Foto, 'id', 'DESC']],
         include: {
           model: Foto,
-          attributes: ['filename'],
+          attributes: ['url', 'filename'],
         },
       },
     );
@@ -64,7 +64,7 @@ class AlunoController {
           order: [['id', 'DESC'], [Foto, 'id', 'DESC']],
           include: {
             model: Foto,
-            attributes: ['filename'],
+            attributes: ['url', 'filename'],
           },
         },
       );
