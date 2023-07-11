e52bbc2271dfab92a64708567767c81855ff986f
Leonardo Santos <leonardojosantos@gmail.com>
2023-07-11 15:26:03 -0300

    Redux Saga

--- Secao_15-React_Redux_ReduxSaga_React_Hooks_e_componentes_funcionais-AVANCADO/react_base_projeto/.eslintrc.js
+++ Secao_15-React_Redux_ReduxSaga_React_Hooks_e_componentes_funcionais-AVANCADO/react_base_projeto/.eslintrc.js
@@ -27,7 +27,7 @@ module.exports = {
     'import/prefer-default-export': 0,
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
-    // 'import/no-extraneous-dependencies': 'off',
+    'import/no-extraneous-dependencies': 'off',
   },
 };
 
--- Secao_15-React_Redux_ReduxSaga_React_Hooks_e_componentes_funcionais-AVANCADO/react_base_projeto/src/store/index.js
+++ Secao_15-React_Redux_ReduxSaga_React_Hooks_e_componentes_funcionais-AVANCADO/react_base_projeto/src/store/index.js
@@ -1,7 +1,12 @@
-import { legacy_createStore as createStore } from 'redux';
+import { legacy_createStore as createStore, applyMiddleware } from 'redux';
+import createMiddleware from 'redux-saga';
 
 import rootReducer from './modules/rootReducer';
+import rootSaga from './modules/rootSaga';
 
-const store = createStore(rootReducer);
+const sagaMiddleware = createMiddleware();
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
