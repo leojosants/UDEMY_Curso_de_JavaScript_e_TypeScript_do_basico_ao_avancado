811271b0a805bd26e4ba6f008b86c99f6b62f7ab
Leonardo Santos <leonardojosantos@gmail.com>
2023-07-11 12:17:02 -0300

    Separando Actions e Reducers

--- Secao_15-React_Redux_ReduxSaga_React_Hooks_e_componentes_funcionais-AVANCADO/react_base_projeto/src/pages/Login/index.js
+++ Secao_15-React_Redux_ReduxSaga_React_Hooks_e_componentes_funcionais-AVANCADO/react_base_projeto/src/pages/Login/index.js
@@ -4,12 +4,14 @@ import { useDispatch } from 'react-redux';
 import { Title, Paragrafo } from './styled';
 import { Container } from '../../styles/GlobalStyles';
 
+import * as exampleActions from '../../store/modules/example/actions';
+
 export default function Login() {
   const dispatch = useDispatch();
 
   function handleClick(e) {
     e.preventDefault();
-    dispatch({ type: 'BOTAO_CLICADO' });
+    dispatch(exampleActions.clicaBotao());
   }
 
   return (
