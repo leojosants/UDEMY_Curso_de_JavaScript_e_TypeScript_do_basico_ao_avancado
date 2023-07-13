e8cb0f8516291680cd3cf1c173a80e4874b3e9c0
Leonardo Santos <leonardojosantos@gmail.com>
2023-07-12 22:13:35 -0300

    Edição dos dados do usuário logado - Parte 1

--- Secao_15-React_Redux_ReduxSaga_React_Hooks_e_componentes_funcionais-AVANCADO/consumir_api/src/config/colors.js
+++ Secao_15-React_Redux_ReduxSaga_React_Hooks_e_componentes_funcionais-AVANCADO/consumir_api/src/config/colors.js
@@ -1,4 +1,4 @@
-export const primaryColor = '#F2aF29';
+export const primaryColor = '#C3073F';
 export const primaryDarkColor = '#1A1A1D';
 export const successColor = '#0197F6';
 export const infoColor = '#0197F6';
--- Secao_15-React_Redux_ReduxSaga_React_Hooks_e_componentes_funcionais-AVANCADO/consumir_api/src/store/modules/auth/sagas.js
+++ Secao_15-React_Redux_ReduxSaga_React_Hooks_e_componentes_funcionais-AVANCADO/consumir_api/src/store/modules/auth/sagas.js
@@ -20,13 +20,20 @@ function* loginRequest({ payload }) {
     yield put(actions.loginFailure());
   }
 }
+
 function persistRehydrate({ payload }) {
   const token = get(payload, 'auth.token', '');
   if (!token) return;
   axios.defaults.headers.Authorization = `Bearer ${token}`;
 }
 
+function registerRequest({ payload }) {
+  const { id, nome, email, password } = payload;
+  console.log('Continua...');
+}
+
 export default all([
   takeLatest(types.LOGIN_REQUEST, loginRequest),
   takeLatest(types.PERSIST_REHYDRATE, persistRehydrate),
+  takeLatest(types.REGISTER_REQUEST, registerRequest),
 ]);
