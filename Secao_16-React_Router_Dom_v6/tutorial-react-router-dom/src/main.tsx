7043495702e09386b46ccee6481d7b08c3cc991c
Leonardo Santos <leonardojosantos@gmail.com>
2023-07-14 17:17:43 -0300

    Usando Links: 'Link' vs 'a'

--- Secao_16-React_Router_Dom_v6/tutorial-react-router-dom/src/main.tsx
+++ Secao_16-React_Router_Dom_v6/tutorial-react-router-dom/src/main.tsx
@@ -5,11 +5,14 @@ import './styles/global.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './components/Home'
 import { About } from './components/About';
+import { Menu } from './components/Menu';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+
     <BrowserRouter>
+      <Menu />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
