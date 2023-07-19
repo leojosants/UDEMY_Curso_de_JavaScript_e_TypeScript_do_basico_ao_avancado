8e5e6bb727a49d8f84753087a8a34a505dcff64d
Leonardo Santos <leonardojosantos@gmail.com>
2023-07-19 13:09:34 -0300

    Exclusão de seção: conflito node_modules

--- Secao_19-TypeScript-Tipos_basicos/src/aula_20-This_em_function_e_arrow_function/index.ts
+++
@@ -1,7 +0,0 @@
-export function funcao(this: Date, name: string, age: number): void {
-  console.log(this);
-  console.log(name, age);
-};
-
-funcao.call(new Date(), 'Leonardo', 39);
-funcao.apply(new Date(), ['Leonardo', 39]);
