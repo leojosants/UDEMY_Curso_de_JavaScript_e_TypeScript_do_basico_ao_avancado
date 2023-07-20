eb012a9b8fdc3dbb5cc20ca63faba5a7351234d4
Leonardo Santos <leonardojosantos@gmail.com>
2023-07-20 15:24:17 -0300

    Union Types

---
+++ Secao-TypeScript/src/aula_0011-Union_Types/index.ts
@@ -0,0 +1,7 @@
+export function addOrConcat(a: number | string, b: number | string): number | string {
+  if (typeof a === 'number' && typeof b === 'number') return a + b;
+  return `${a}${b}`;
+}
+
+console.log(addOrConcat(3, 5));
+console.log(addOrConcat('a', 'b'));
