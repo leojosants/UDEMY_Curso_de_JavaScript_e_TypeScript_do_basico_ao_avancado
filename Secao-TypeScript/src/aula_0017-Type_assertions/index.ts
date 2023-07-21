d08de397cc72ab23518f3740bddb3e9c0ea74a4c
Leonardo Santos <leonardojosantos@gmail.com>
2023-07-21 19:59:22 -0300

    Type assertions

---
+++ Secao-TypeScript/src/aula_0017-Type_assertions/index.ts
@@ -0,0 +1,24 @@
+/* Recommended */
+// Conditional
+const body1 = document.querySelector('body');
+if (body1) body1.style.background = 'red';
+
+/* Recommended */
+// Type assertion
+const body3 = document.querySelector('body') as HTMLBodyElement;
+body3.style.background = 'red';
+
+/* Recommended */
+const input = document.querySelector('.input') as HTMLInputElement;
+input.value = 'Qualquer coisa';
+input.focus();
+
+/* Not recommended */
+// Non-null assertion (!)
+const body2 = document.querySelector('body')!;
+body2.style.background = 'red';
+
+/* Not recommended */
+// Type assertion
+// eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+const body4 = (document.querySelector('body') as unknown) as number;
