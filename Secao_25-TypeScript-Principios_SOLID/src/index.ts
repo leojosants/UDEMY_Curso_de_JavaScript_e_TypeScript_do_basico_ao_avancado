7c7c2759fefc5f134565dcf99368f52efbee79cb
Leonardo Santos <leonardojosantos@gmail.com>
2023-07-26 16:16:32 -0300

    Vantagens, desvantagens DRY, YAGNI, KISS, SOC, MVP

---
+++ Secao_25-TypeScript-Principios_SOLID/src/index.ts
@@ -0,0 +1,30 @@
+/*
+  Vantagens e desvantagens do S.O.L.I.D.
+
+    S   Single-responsibility principle
+    O   Open–closed principle
+    L   Liskov substitution principle
+    I   Interface segregation principle
+    D   Dependency inversion principle
+
+  - Vantagens:
+    - Código modular.
+    - Código reutilizável (D.R.Y - Don't repeat yourself).
+    - Código testável, baixo acoplamento.
+    - Baixo acoplamento e alta coesão.
+    - Código expansível.
+    - Separations of concerns (Separação de conceitos).
+    - Fácil manutenção.
+
+  - Desvantagens:
+    - Complexidade.
+    - Quantidade de código digitado aumenta.
+    - Tempo de desenvolvimento aumenta bastante.
+    - Tenha cuidados com: YAGNI (You aren't gonna need it), KISS (Keep it simple, stupid!)
+
+      Y   A       G     N     I
+      You aren't  gonna need  it
+
+      K     I   S       S
+      Keep  it  simple, stupid!
+*/
