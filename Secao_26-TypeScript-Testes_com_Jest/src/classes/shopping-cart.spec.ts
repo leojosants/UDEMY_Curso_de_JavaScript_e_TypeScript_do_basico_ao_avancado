0482e0dbc547fa253b807df5e24ab750c8eaacd2
Leonardo Santos <leonardojosantos@gmail.com>
2023-07-26 22:34:07 -0300

    Testando Shopping-Cart.ts com Mocks - Parte 1

---
+++ Secao_26-TypeScript-Testes_com_Jest/src/classes/shopping-cart.spec.ts
@@ -0,0 +1,8 @@
+import { ShoppingCart } from './shopping-cart';
+
+describe('ShoppingCart', () => {
+  it('should be an empty cart when no product is add', () => {
+    class DiscoutMock
+    const sut = new ShoppingCart();
+  });
+});
